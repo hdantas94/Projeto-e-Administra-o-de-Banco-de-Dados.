1ad9b4f11335f76ee2263ed82d6bce027fe13983
Hilário Dantas <77644127+hdantas94@users.noreply.github.com>
2022-05-09 08:10:32 -0300

    Update tarefa01-create.sql
    inserindo tabelas

--- tarefa01-create.sql
+++ tarefa01-create.sql
@@ -1,1 +1,48 @@
+SET FOREIGN_KEY_CHECKS=1; 
 
+drop table if exists atividade cascade;
+drop table if exists projeto cascade;
+drop table if exists departamento cascade;
+drop table if exists funcionario cascade;
+
+CREATE TABLE funcionario (
+	codigo int AUTO_INCREMENT,
+	nome varchar(50),
+	sexo char(1),
+	dataNasc date,
+	salario decimal(10,2),
+	codDepto int,
+	PRIMARY KEY (codigo),
+	FOREIGN KEY (codDepto) REFERENCES funcionario(codigo) on delete set null on update cascade
+);
+
+CREATE TABLE departamento (
+	codigo int AUTO_INCREMENT,
+	descricao varchar(50),
+	codGerente int,
+	PRIMARY KEY (codigo),
+	FOREIGN KEY (codGerente) REFERENCES funcionario(codigo) on delete set null on update cascade
+);
+
+CREATE TABLE projeto (
+	codigo int AUTO_INCREMENT,
+	nome varchar(50),
+	descricao varchar(250),
+	codDepto int,
+  codResponsavel int,
+	dataInicio date, 
+	dataFim date,
+	PRIMARY KEY (codigo),
+	FOREIGN KEY (codResponsavel) REFERENCES funcionario(codigo) on delete set null on update cascade,
+	FOREIGN KEY (codDepto) REFERENCES departamento(codigo) on delete set null on update cascade
+);
+
+CREATE TABLE atividade (
+	codigo int AUTO_INCREMENT,
+	descricao varchar(250),
+	codResponsavel int,
+	dataInicio date, 
+	dataFim date,
+	PRIMARY KEY (codigo),
+	FOREIGN KEY (codResponsavel) REFERENCES funcionario(codigo) on delete set null on update cascade
+  );
