b6105e548911e7fe10bf8ee8ca5e9cd67f543201
Hilário Dantas <77644127+hdantas94@users.noreply.github.com>
2022-05-09 08:21:16 -0300

    Update tarefa01-inserts.sql

--- tarefa01-inserts.sql
+++ tarefa01-inserts.sql
@@ -1,1 +1,19 @@
+insert into funcionario
+(nome, sexo, dtNasc, salario, codDepto)
+values ('Eduardo', 'M', '1958-05-07', 9999.99, null);
 
+insert into funcionario
+(nome, sexo, dtNasc, salario, codDepto)
+values ('Rauan', 'M', '2002-02-10', 5999.99, null);
+
+insert into funcionario
+(nome, sexo, dtNasc, salario, codDepto)
+values ('Renildo', 'M', '1999-09-03', 3999.99, null);
+
+insert into funcionario
+(nome, sexo, dtNasc, salario, codDepto)
+values ('Soneca', 'M', '1990-12-12', 99999.99, null);
+
+insert into funcionario
+(nome, sexo, dtNasc, salario, codDepto)
+values ('Felipe', 'M', '2001-01-01', 1212.00, null);
